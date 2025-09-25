f06a24a8069eae1347330fcbacc510fc9bd03558
rpellya@gmail.com <you@example.com>
2025-09-25 20:31:43 +0300

    Add content for Resume Page, styles and some fix

---
+++ src/components/Resume/Resume.tsx
@@ -0,0 +1,80 @@
+import { classNames } from 'app/lib/classNames';
+import { AppLink } from 'shared/ui/AppLink/AppLink';
+import { ResumeItemCard } from './ResumeItemCard/ResumeItemCard';
+import cls from './Resume.module.scss';
+
+interface ResumeProps {
+    className?: string;
+}
+
+export const Resume: React.FC<ResumeProps> = ({ className }) => {
+    const descriptionSumary =
+        'Frontend-разработчик с 2+ годами опыта. Специализируюсь на создании адаптивных и производительных веб-приложений. Постоянно изучаю современные инструменты и оптимизирую процессы.';
+
+    const descriptionEducationSPO =
+        ' 09.02.07. Программирование и информационные технологии';
+
+    return (
+        <div className={classNames('', {}, [className])}>
+            <span>
+                Я стремлюсь занять позицию middle+, senior
+                frontend-разработчика. Всегда открыт для изучения новых
+                возможностей и применения своих навыков в разработке
+                программного обеспечения. Готов взять на себя ответственность за
+                сложные задачи и внести вклад в создание качественных продуктов.
+            </span>
+            <p>
+                Вы можете скачать моё резюме
+                <AppLink theme="primary" className={cls.link}>
+                    здесь
+                </AppLink>
+            </p>
+            <div className={cls.items}>
+                <ResumeItemCard
+                    title={'Sumary'}
+                    subTitle={'Pellya Roman'}
+                    description={descriptionSumary}
+                    labels={['St. Petersburg, Russia', 'rpellya@gmail.com']}
+                />
+                <ResumeItemCard
+                    className={cls.experience}
+                    title={'Professional Experience'}
+                    subTitle={'Frontend Developer'}
+                    years="2023 - Present"
+                    geoPlace={'Datagile, Russia'}
+                    labels={[
+                        'Создание и поддержка административных панелей для управления крупными продуктами.',
+                        'Реализация сложной бизнес-логики с использованием React, Redux и RTK Query.',
+                        'Участие в полном цикле разработки: от проектирования архитектуры до тестирования и деплоя.',
+                        'Проведение код-ревью и менторинг junior-разработчиков.',
+                        'Внедрение лучших практик (code style, типизация, документация).',
+                        'Оптимизация процессов разработки: сокращение времени сборки на 40% через настройку Webpack/Vite.',
+                        'Доведение покрытия кода тестами до 80%+ для критически важных модулей.',
+                        '3 больших и тяжелых проекта на React/TypeScript:',
+                        'Миграция на TypeScript, рефакторинг кода, создание своей UI-библиотеки для проекта.',
+                        'Настройка unit-тестов (Jest, React Testing Library) и скриншотного тестирования.',
+                    ]}
+                />
+                <ResumeItemCard
+                    title={'Education'}
+                    items={[
+                        {
+                            subTitle:
+                                'Санкт-Петербургский политехнический университет Петра Великого (СПбПУ)',
+                            years: '2024 - Present',
+                            geoPlace: 'St. Petersburg, Russia',
+                            description: '09.03.03. Прикладная инфорамтика',
+                        },
+                        {
+                            subTitle:
+                                'Институт среднего профессионального образования им. Петра Великого',
+                            years: '2020 - 2024',
+                            geoPlace: 'St. Petersburg, Russia',
+                            description: descriptionEducationSPO,
+                        },
+                    ]}
+                />
+            </div>
+        </div>
+    );
+};
---
+++ src/components/Resume/ResumeItemCard/ResumeItemCard.tsx
@@ -0,0 +1,82 @@
+import { classNames } from 'app/lib/classNames';
+import cls from './ResumeItemCard.module.scss';
+
+interface ResumeItemCardProps {
+    title: string;
+    subTitle?: string;
+    description?: string;
+    years?: string;
+    geoPlace?: string;
+    labels?: string[];
+    className?: string;
+    items?: {
+        subTitle?: string;
+        description?: string;
+        years?: string;
+        geoPlace?: string;
+        labels?: string[];
+    }[];
+}
+
+export const ResumeItemCard: React.FC<ResumeItemCardProps> = ({
+    title,
+    subTitle,
+    description,
+    years,
+    geoPlace,
+    labels,
+    className,
+    items,
+}) => {
+    return (
+        <div className={classNames(cls.ResumeItemCard, {}, [className])}>
+            {items ? (
+                <>
+                    <h3 className={cls.title}>{title}</h3>
+                    {items.map((item) => (
+                        <div className={cls.itemCardContainer}>
+                            <h4 className={cls.subTitle}>{item.subTitle}</h4>
+                            {item.years && (
+                                <span className={cls.years}>{item.years}</span>
+                            )}
+                            {item.geoPlace && (
+                                <span className={cls.geoPlace}>
+                                    {item.geoPlace}
+                                </span>
+                            )}
+                            <span>{item.description}</span>
+
+                            <ul className={cls.labelsContainer}>
+                                {item.labels?.map((label, index) => (
+                                    <li key={index} className={cls.label}>
+                                        {label}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </>
+            ) : (
+                <>
+                    <h3 className={cls.title}>{title}</h3>
+                    <div className={cls.itemCardContainer}>
+                        <h4 className={cls.subTitle}>{subTitle}</h4>
+                        {years && <span className={cls.years}>{years}</span>}
+                        {geoPlace && (
+                            <span className={cls.geoPlace}>{geoPlace}</span>
+                        )}
+                        <span>{description}</span>
+
+                        <ul className={cls.labelsContainer}>
+                            {labels?.map((label, index) => (
+                                <li key={index} className={cls.label}>
+                                    {label}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </>
+            )}
+        </div>
+    );
+};
